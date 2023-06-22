ecd8aa356161bb99a0b2e2e0a1f52dca7ec05d5c
MARIA EDUARDA CHEHADE JACQUES <1134607@atitus.local>
2023-06-21 21:42:21 -0300

    Código base

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("Hello")
+import pygame
+from tkinter import simpledialog
+pygame.init()
+
+#configurações:
+tamanho=(600,600)
+display=pygame.display.set_mode(tamanho)
+pygame.display.set_caption("Space Marker")
+
+#cores:
+branco=(255,255,255)
+preto=(0,0,0)
+azul=(143, 174, 171)
+roxo=(139,0,139)
+
+coordenadas=[]
+
+#Jogo:
+running=True
+while running:
+    display.fill(azul)
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+        elif event.type==pygame.MOUSEBUTTONUP:
+            pos=pygame.mouse.get_pos()
+            item=simpledialog.askstring("Space", "Qual o nome dessa estrela?")
+            coordenadas.append(pos)
+            print(item)
+            raio=10
+            if item==None:
+                item="Estrela desconhecida"+str(pos)
+                print(item)
+    for item in coordenadas:
+        pygame.draw.circle(display, roxo, item, raio)
+
+    
+    pygame.display.update()
+pygame.quit()
