f1aafde08e97887072870bacccde8cf5faafb2ec
MariaChehade <134561878+MariaChehade@users.noreply.github.com>
2023-06-28 17:32:15 -0300

    Add files via upload
    Adição de texto e ajuste de tela que o git não quer enviar pro github

--- main.py
+++ main.py
@@ -1,39 +1,70 @@
 import pygame
-from tkinter import simpledialog
+from tkinter import simpledialog 
+from pygame.locals import *
+from sys import exit
+from random import randint
 pygame.init()
-
-#configurações:
-tamanho=(600,600)
-display=pygame.display.set_mode(tamanho)
-pygame.display.set_caption("Space Marker")
 
 #cores:
 branco=(255,255,255)
 preto=(0,0,0)
 azul=(143, 174, 171)
 roxo=(139,0,139)
+vermelho=(51,0,0)
 
+#configurações:
+tamanho=(800,550)
+display=pygame.display.set_mode(tamanho)
+pygame.display.set_caption("Space Marker")
+fundo=pygame.image.load('fundo.jpeg')
+som=pygame.mixer.Sound("som.mp3")
 coordenadas=[]
+texto=[]
+raio=10
+fonte=pygame.font.SysFont("arial",40,True,False)
+pontos=0
+pygame.init()
+pygame.font.init()
+
+
+
+display.fill(vermelho)
+display.blit(fundo, (0,100))
+pygame.mixer.Sound.play(som)
+item=[]
+    
+
+
+
+
 
 #Jogo:
 running=True
 while running:
-    display.fill(azul)
+
+
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
         elif event.type==pygame.MOUSEBUTTONUP:
             pos=pygame.mouse.get_pos()
             item=simpledialog.askstring("Space", "Qual o nome dessa estrela?")
+            textoForma=fonte.render(item,True, branco)
             coordenadas.append(pos)
+            texto.append(item)
             print(item)
-            raio=10
+            
             if item==None:
                 item="Estrela desconhecida"+str(pos)
                 print(item)
-    for item in coordenadas:
-        pygame.draw.circle(display, roxo, item, raio)
 
-    
-    pygame.display.update()
+    coordenadas_copy=coordenadas.copy()
+    for item in coordenadas_copy:
+        pygame.draw.circle(display, preto,  item, raio+4)
+        pygame.draw.circle(display, branco, item, raio)
+        texto_surface = fonte.render("oi", True, branco)
+
+        display.blit(textoForma, (pos)) 
+        
+    pygame.display.flip()
 pygame.quit()
